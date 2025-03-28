a2d5fe3ae782a8d6e4aa83f8a45977968ae970ea
hemkersherlock <121245873+hemkersherlock@users.noreply.github.com>
2025-03-28 16:22:25 +0530

    Update CreateToken.tsx

--- src/components/CreateToken.tsx
+++ src/components/CreateToken.tsx
@@ -1,212 +1,124 @@
-import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import {
-  Keypair,
-  PublicKey,
-  SystemProgram,
-  Transaction,
-} from "@solana/web3.js";
-import {
-  MINT_SIZE,
-  TOKEN_PROGRAM_ID,
-  createInitializeMintInstruction,
-  getMinimumBalanceForRentExemptMint,
-} from "@solana/spl-token";
-import {
-  createCreateMetadataAccountV3Instruction,
-  PROGRAM_ID,
-} from "@metaplex-foundation/mpl-token-metadata";
-import { FC, useCallback, useState } from "react";
-import { notify } from "utils/notifications";
-import { ClipLoader } from "react-spinners";
-import { useNetworkConfiguration } from "contexts/NetworkConfigurationProvider";
+import { FC, useCallback, useState } from 'react';
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { Keypair, PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
+import { MINT_SIZE, TOKEN_PROGRAM_ID, createInitializeMintInstruction, getMinimumBalanceForRentExemptMint, getAssociatedTokenAddress, createAssociatedTokenAccountInstruction, createMintToInstruction } from '@solana/spl-token';
+import { createCreateMetadataAccountV3Instruction, PROGRAM_ID } from '@metaplex-foundation/mpl-token-metadata';
 
 export const CreateToken: FC = () => {
   const { connection } = useConnection();
   const { publicKey, sendTransaction } = useWallet();
-  const { networkConfiguration } = useNetworkConfiguration();
+  const [tokenName, setTokenName] = useState('')
+  const [symbol, setSymbol] = useState('')
+  const [metadata, setMetadata] = useState('')
+  const [amount, setAmount] = useState('')
+  const [decimals, setDecimals] = useState('')
 
-  const [tokenName, setTokenName] = useState("");
-  const [tokenSymbol, setTokenSymbol] = useState("");
-  const [tokenUri, setTokenUri] = useState("");
-  const [tokenDecimals, setTokenDecimals] = useState("9");
-  const [tokenMintAddress, setTokenMintAddress] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const onClick = useCallback(async (form) => {
+      const lamports = await getMinimumBalanceForRentExemptMint(connection);
+      const mintKeypair = Keypair.generate();
+      const tokenATA = await getAssociatedTokenAddress(mintKeypair.publicKey, publicKey);
 
-  const createToken = useCallback(async () => {
-    if (!publicKey) {
-      notify({ type: "error", message: `Wallet not connected!` });
-      return;
-    }
-
-    const lamports = await getMinimumBalanceForRentExemptMint(connection);
-    const mintKeypair = Keypair.generate();
-
-    setIsLoading(true);
-    try {
-      const tx = new Transaction().add(
-        SystemProgram.createAccount({
-          fromPubkey: publicKey,
-          newAccountPubkey: mintKeypair.publicKey,
-          space: MINT_SIZE,
-          lamports,
-          programId: TOKEN_PROGRAM_ID,
-        }),
-
-        createInitializeMintInstruction(
-          mintKeypair.publicKey,
-          Number(tokenDecimals),
-          publicKey,
-          publicKey,
-          TOKEN_PROGRAM_ID,
-        ),
-
-        createCreateMetadataAccountV3Instruction(
-          {
-            metadata: (
-              await PublicKey.findProgramAddress(
-                [
-                  Buffer.from("metadata"),
-                  PROGRAM_ID.toBuffer(),
-                  mintKeypair.publicKey.toBuffer(),
-                ],
-                PROGRAM_ID,
-              )
-            )[0],
-            mint: mintKeypair.publicKey,
-            mintAuthority: publicKey,
-            payer: publicKey,
-            updateAuthority: publicKey,
-          },
-          {
-            createMetadataAccountArgsV3: {
-              data: {
-                name: tokenName,
-                symbol: tokenSymbol,
-                uri: tokenUri,
-                creators: null,
-                sellerFeeBasisPoints: 0,
-                collection: null,
-                uses: null,
-              },
-              isMutable: false,
-              collectionDetails: null,
+      const createMetadataInstruction = createCreateMetadataAccountV3Instruction(
+        {
+          metadata: PublicKey.findProgramAddressSync(
+            [
+              Buffer.from("metadata"),
+              PROGRAM_ID.toBuffer(),
+              mintKeypair.publicKey.toBuffer(),
+            ],
+            PROGRAM_ID,
+          )[0],
+          mint: mintKeypair.publicKey,
+          mintAuthority: publicKey,
+          payer: publicKey,
+          updateAuthority: null,
+        },
+        {
+          createMetadataAccountArgsV3: {
+            data: {
+              name: form.tokenName,
+              symbol: form.symbol,
+              uri: form.metadata,
+              creators: null,
+              sellerFeeBasisPoints: 0,
+              uses: null,
+              collection: null,
             },
+            isMutable: false,
+            collectionDetails: null,
           },
-        ),
+        },
       );
-      const signature = await sendTransaction(tx, connection, {
-        signers: [mintKeypair],
-      });
-      setTokenMintAddress(mintKeypair.publicKey.toString());
-      notify({
-        type: "success",
-        message: "Token creation successful",
-        txid: signature,
-      });
-    } catch (error: any) {
-      notify({ type: "error", message: "Token creation failed" });
-    }
-    setIsLoading(false);
-  }, [
-    publicKey,
-    connection,
-    tokenDecimals,
-    tokenName,
-    tokenSymbol,
-    tokenUri,
-    sendTransaction,
-  ]);
+
+      const createNewTokenTransaction = new Transaction().add(
+        SystemProgram.createAccount({
+            fromPubkey: publicKey,
+            newAccountPubkey: mintKeypair.publicKey,
+            space: MINT_SIZE,
+            lamports: lamports,
+            programId: TOKEN_PROGRAM_ID,
+        }),
+        createInitializeMintInstruction(
+          mintKeypair.publicKey, 
+          form.decimals, 
+          publicKey, 
+          publicKey, 
+          TOKEN_PROGRAM_ID),
+        createAssociatedTokenAccountInstruction(
+          publicKey,
+          tokenATA,
+          publicKey,
+          mintKeypair.publicKey,
+        ),
+        createMintToInstruction(
+          mintKeypair.publicKey,
+          tokenATA,
+          publicKey,
+          form.amount * Math.pow(10, form.decimals),
+        ),
+        createMetadataInstruction
+      );
+      await sendTransaction(createNewTokenTransaction, connection, {signers: [mintKeypair]});
+  }, [publicKey, connection, sendTransaction]);
 
   return (
-    <div>
-      {isLoading && (
-        <div className="absolute top-0 left-0 z-50 flex h-screen w-full items-center justify-center bg-black/[.3] backdrop-blur-[10px]">
-          <ClipLoader />
-        </div>
-      )}
-      {!tokenMintAddress ? (
-        <div>
-          <div className="mt-4 sm:grid sm:grid-cols-2 sm:gap-4">
-            <div className="m-auto p-2 text-xl font-normal">Token name</div>
-            <div className="m-auto p-2">
-              <input
-                className="rounded border px-4 py-2 text-xl font-normal text-gray-700 focus:border-blue-600 focus:outline-none"
-                onChange={(e) => setTokenName(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className="mt-4 sm:grid sm:grid-cols-2 sm:gap-4">
-            <div className="m-auto p-2">
-              <div className="text-xl font-normal">Token symbol</div>
-              <p>{"Abbreviated name (e.g. Solana -> SOL)."}</p>
-            </div>
-            <div className="m-auto p-2">
-              <input
-                className="rounded border px-4 py-2 text-xl font-normal text-gray-700 focus:border-blue-600 focus:outline-none"
-                onChange={(e) => setTokenSymbol(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className="mt-4 sm:grid sm:grid-cols-2 sm:gap-4">
-            <div className="m-auto p-2">
-              <div className="text-xl font-normal">Token URI</div>
-              <p>Link to your metadata json file.</p>
-              <p>
-                Paste an existing one or create new
-                <a
-                  className="cursor-pointer font-medium text-purple-500 hover:text-indigo-500"
-                  href="./upload"
-                >
-                  {" here"}
-                </a>
-                .
-              </p>
-              <p>You can leave it blank if you don`t need token image.</p>
-            </div>
-            <div className="m-auto p-2">
-              <input
-                className="rounded border px-4 py-2 text-xl font-normal text-gray-700 focus:border-blue-600 focus:outline-none"
-                onChange={(e) => setTokenUri(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className="mt-4 sm:grid sm:grid-cols-2 sm:gap-4">
-            <div className="m-auto p-2">
-              <div className="text-xl font-normal">Token decimals</div>
-              <p>Default value is 9 for solana.</p>
-            </div>
-            <div className="m-auto p-2">
-              <input
-                className="rounded border px-4 py-2 text-xl font-normal text-gray-700 focus:border-blue-600 focus:outline-none"
-                type={"number"}
-                min={0}
-                value={tokenDecimals}
-                onChange={(e) => setTokenDecimals(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className="mt-4">
-            <button
-              className="... btn m-2 animate-pulse bg-gradient-to-r from-[#9945FF] to-[#14F195] px-8 hover:from-pink-500 hover:to-yellow-500"
-              onClick={createToken}
-            >
-              Create token
-            </button>
-          </div>
-        </div>
-      ) : (
-        <div className="mt-4 break-words">
-          <p className="font-medium">Link to your new token.</p>
-          <a
-            className="cursor-pointer font-medium text-purple-500 hover:text-indigo-500"
-            href={`https://explorer.solana.com/address/${tokenMintAddress}?cluster=${networkConfiguration}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            {tokenMintAddress}
-          </a>
-        </div>
-      )}
+    <div className="my-6">
+      <input
+        type="text"
+        className="form-control block mb-2 w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+        placeholder="Token Name"
+        onChange={(e) => setTokenName(e.target.value)}
+      />
+      <input
+        type="text"
+        className="form-control block mb-2 w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+        placeholder="Symbol"
+        onChange={(e) => setSymbol(e.target.value)}
+      />
+      <input
+        type="text"
+        className="form-control block mb-2 w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+        placeholder="Metadata Url"
+        onChange={(e) => setMetadata(e.target.value)}
+      />
+      <input
+        type="number"
+        className="form-control block mb-2 w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+        placeholder="Amount"
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <input
+        type="number"
+        className="form-control block mb-2 w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+        placeholder="Decimals"
+        onChange={(e) => setDecimals(e.target.value)}
+      />
+      
+      <button
+        className="px-8 m-2 btn animate-pulse bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ..."
+        onClick={() => onClick({decimals: Number(decimals), amount: Number(amount), metadata: metadata, symbol: symbol, tokenName: tokenName})}>
+          <span>Create Token</span>
+      </button>
     </div>
-  );
-};
+  )
+}
